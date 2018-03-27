2434f169db4b9ab6e6fb9e19541895b72d392e81
cybernetck <mattswomack@gmail.com>
2018-03-27 16:13:40 -0500

    adding client id to test example to explore functionality

--- src/vendor/config.js
+++ src/vendor/config.js
@@ -1,7 +1,8 @@
 const config = {
-	publicId: 'd5BY7IIJYsyoSrabACClkacWlB21xCULA53dSMyiX5',
+	//publicId: 'd5BY7IIJYsyoSrabACClkacWlB21xCULA53dSMyiX5',
+	publicId: 'XBhWG5uOk67N9EX4dSKoyrYC5JzVlPyfh6FAYFZm6i'
 	// The content from this category is displayed in the upper full width box
-	featuredCategoryId: '1415212879321235847'
+	//featuredCategoryId: '1415212879321235847'
 };
 
 export default config;
